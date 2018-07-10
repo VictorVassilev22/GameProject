86bdfea89cea049ed812e7e91e20a783e749b3a0
VictorVassilev22 <victor.dimitrov.vasilev@gmail.com>
2018-07-10 22:35:51 +0300

    Added basic scene

---
+++ Game 0.0.0/Assets/Scripts/Accelerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(Input.acceleration.x, 0, 0);
+	}
+}
