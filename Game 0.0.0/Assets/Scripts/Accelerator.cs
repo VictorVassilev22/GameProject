4d3a9646290f2cf0a0b2eb48843b17d5888e5318
VictorVassilev22 <victor.dimitrov.vasilev@gmail.com>
2018-07-12 00:18:44 +0300

    Borders done

--- Game 0.0.0/Assets/Scripts/Accelerator.cs
+++ Game 0.0.0/Assets/Scripts/Accelerator.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Accelerator : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-        this.transform.Translate(Input.acceleration.x, 0, 0);
+    public float speed = 250f;
+    private Rigidbody rbody;
+    private float horiz;
+
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update () {
+        horiz = Input.GetAxis("Horizontal");
+#if UNITY_ANDROID
+        horiz = Input.acceleration.x;
+#endif
+
+        this.rbody.velocity= new Vector2(horiz* speed*Time.deltaTime, 0);
 	}
 }
