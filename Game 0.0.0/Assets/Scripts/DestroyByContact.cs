d5cf5b15f3ea8319e1c7f6ec200a533ded06b993
Todor <tosheto99.bg@abv.bg>
2018-07-13 23:40:25 +0300

    Added Enemy

---
+++ Game 0.0.0/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        //Ako tova go nqma kato cukna Play mi premahva gameobject-a i Banudary-to ,toest dwata Cube-a.Vidqh v internet kak go oprawqt
+        if (other.tag == "Boundary") 
+        { 
+            return;
+        }
+
+        Destroy(other.gameObject); //- Destroys the ball when hit the Enemy
+        Destroy(gameObject); //-Destroys the enemy
+    }
+}
