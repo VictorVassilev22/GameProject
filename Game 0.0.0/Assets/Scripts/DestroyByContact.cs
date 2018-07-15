362c9df627f4500dcb8de00c629fb20b0335c9e5
Todor <tosheto99.bg@abv.bg>
2018-07-15 14:35:25 +0300

    Enemy is moving again

--- Game 0.0.0/Assets/Scripts/DestroyByContact.cs
+++ Game 0.0.0/Assets/Scripts/DestroyByContact.cs
@@ -3,15 +3,10 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject enemy;
     void OnTriggerEnter(Collider other)
     {
-        //Ako tova go nqma kato cukna Play mi premahva gameobject-a i Banudary-to ,toest dwata Cube-a.Vidqh v internet kak go oprawqt
-        if (other.tag == "Boundary") 
-        { 
-            return;
-        }
-
         Destroy(other.gameObject); //- Destroys the ball when hit the Enemy
-        Destroy(gameObject); //-Destroys the enemy
+        Destroy(enemy); //-Destroys the enemy
     }
 }
