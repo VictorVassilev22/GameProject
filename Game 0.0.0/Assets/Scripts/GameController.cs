3dcb06c7fc1856bc5dc4ce7d81fc76067372071d
Todor <tosheto99.bg@abv.bg>
2018-07-17 17:38:39 +0300

    Spawn

---
+++ Game 0.0.0/Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    void Start()
+    {
+        SpawnWaves();
+    }
+    void SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range (-spawnValues.x ,spawnValues.x) ,spawnValues.y ,spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
