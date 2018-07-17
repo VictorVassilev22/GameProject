3dfdc75b5cdbf58fbd107340ad2359622fa7c1be
Todor <tosheto99.bg@abv.bg>
2018-07-17 12:39:27 +0300

    Explosion animation

--- Game 0.0.0/Assets/Scripts/DestroyByContact.cs
+++ Game 0.0.0/Assets/Scripts/DestroyByContact.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
     void OnCollisionEnter2D(Collision2D col) // -the collision function and parameter must be for 2D
     {
         if (col.gameObject.name == "missile(Clone)") // check if the object colliding is the missle
         {
+            Instantiate(explosion, transform.position, transform.rotation);
             Destroy(col.gameObject);//destroy the missle
             Destroy(this.gameObject); //destroy the enemy
         }
