7fa6a674d87873a3121277112df8a0df7a281b83
Todor <tosheto99.bg@abv.bg>
2018-07-23 23:34:59 +0300

    ColorScript

---
+++ Game 0.0.0/Assets/Scripts/PlayerChangeColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChangeColor : MonoBehaviour {
+ void OnTriggerEnter2D(Collider2D col)
+     {
+         if(col.gameObject.tag == "Enemy")
+         {
+             col.gameObject.GetComponent<Renderer>().material.color = Color.red;
+         }
+     }
+
+}
