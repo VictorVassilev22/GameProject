2910fcc20d71df424c3e7dad4188b25b77b3653f
Sentinel <victor.dimitrov.vasilev@gmail.com>
2019-07-05 18:03:50 +0300

    enemies have health bars

---
+++ Game 0.0.0/Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    private Transform bar;
+    private float maxHealth;
+    private EnemyScript enemy;
+    private float scaleX;
+    private float scaleY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = this.gameObject.GetComponent<EnemyScript>();
+        bar = this.transform.GetChild(2).GetChild(1);
+        maxHealth = enemy.health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bar.localScale = new Vector3(enemy.health / maxHealth, 1);
+    }
+}
