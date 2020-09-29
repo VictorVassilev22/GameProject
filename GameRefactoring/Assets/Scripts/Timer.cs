959ee9301d9c5e9bc12b0cfb7ab106aea62bbaaf
VictorVassilev22 <victor.dimitrov.vasilev@gmail.com>
2020-09-29 23:37:31 +0300

    Added timer script

--- GameRefactoring/Assets/Scripts/Timer.cs
+++ GameRefactoring/Assets/Scripts/Timer.cs
@@ -1,18 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    #region Fields
+
+    // if we have a threshold where to stop (like a chronometer)
+    [SerializeField]
+    bool isFixed = false;
+
+    // value of the threshold
+    [SerializeField]
+    float fixedTime = -1f;
+
+    //is the timer running
+    bool isRunning = false;
+    //how many time has passed
+    float timePassed = 0f;
+
+    #endregion
+
+    #region Properties
+    public bool IsRunning
     {
+        get { return isRunning; }
+    }
+
+    public bool IsFixed
+    {
+        get { return IsFixed; }
+    }
+
+    public float FixedTime
+    {
+        get { return isFixed ? fixedTime : -1f; }
+    }
+
+    public float TimePassed
+    {
+        get { return timePassed; }
+    }
+
+
+
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// adding time every frame
+    /// time.deltaTime returns how many seconds it took for the last frame to execute
+    /// </summary>
+    private void Update()
+    {
+
+        if (isFixed)
+        {
+            if (timePassed >= fixedTime)
+            {
+                Pause();
+            }
+        }
+
+        if(isRunning)
+            timePassed += Time.deltaTime;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Pause()
     {
-        
+        isRunning = false;
     }
+
+    public void Run()
+    {
+        if (isFixed && timePassed >= fixedTime)
+            return;
+            
+        isRunning = true;
+    }
+
+    #endregion
+
+
 }
